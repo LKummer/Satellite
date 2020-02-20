772758cdedb41f62172da895e784f99a71f973c3
Lior Kummer <LKummer41@gmail.com>
2020-02-20 17:26:45 +0200

    Add Sass processing with CSS file extraction.

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,30 @@
-const path = require('path')
-const CopyPlugin = require('copy-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     mode: 'development',
     output: {
-        filename: 'theme/static/main.js'
+        filename: 'theme/static/[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
     },
     plugins: [
         new CopyPlugin([
             { from: 'layouts', to: 'theme/layouts' },
             { from: 'public', to: 'theme' }
-        ])
+        ]),
+        new MiniCssExtractPlugin({
+            filename: "theme/static/[name].css"
+        })
     ]
 }
