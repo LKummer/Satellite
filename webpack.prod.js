633912501c2ab269f98bf4b985da71c8280ec036
Lior Kummer <LKummer41@gmail.com>
2020-02-22 01:11:04 +0200

    Configure minification for production build.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,22 @@
 const WebpackRTLPlugin = require('webpack-rtl-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
     mode: 'production',
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                parallel: true,
+                terserOptions: {
+                    ecma: 8
+                }
+            })
+        ]
+    },
     plugins: [
-        new WebpackRTLPlugin()
+        new WebpackRTLPlugin(),
+        new OptimizeCssAssetsPlugin()
     ]
 }
