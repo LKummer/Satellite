4fc7930acf0c56be6b8b58dd31efa6afb639a159
Lior Kummer <LKummer41@gmail.com>
2020-07-16 18:25:51 +0300

    Refactor the Webpack configuration Remove configuration merging to simplify the configuration. Refactor to a single configuration. Remove extra minification to simplify the production configuration. Fix hot reload inconsistencies by watching the layouts folder with Hugo. Refactor the Webpack configuration to not copy layouts in hot reload.

--- webpack.config.js
+++ webpack.config.js
@@ -2,65 +2,59 @@ const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const WebpackRTLPlugin = require('webpack-rtl-plugin');
 const StylelintPlugin = require('stylelint-webpack-plugin');
-const merge = require('webpack-merge');
 
-// Configuration for building a Hugo theme in dist/theme.
-const build = {
-  output: {
-    filename: 'theme/static/[name].js'
-  },
-  module: {
-    rules: [
-      {
-        // Pass JS files through ESLint.
-        test: /\.js$/iu,
-        exclude: /node_modules/u,
-        loader: 'eslint-loader'
-      },
-      {
-        // Process SCSS and extract a CSS file.
-        test: /\.(?:css|scss|sass)$/iu,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-      },
-      {
-        // Load fonts.
-        test: /\.woff2?$/u,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-          outputPath: 'theme/static/fonts',
-          publicPath: './fonts/'
-        }
-      }
-    ]
-  },
-  plugins: [
-    // Copies files to the dist/theme folder.
-    new CopyPlugin([
-      { from: 'layouts', to: 'theme/layouts' },
-      { from: 'i18n', to: 'theme/i18n' },
-      { from: 'public', to: 'theme' }
-    ]),
-    // Extracts a CSS file.
-    new MiniCssExtractPlugin({
-      filename: 'theme/static/[name].css'
-    }),
-    // Extracts an RTL processed CSS file.
-    new WebpackRTLPlugin(),
-    // Lints stylesheets.
-    new StylelintPlugin({
-      syntax: 'scss'
-    })
-  ]
-};
-
-// Merge the configurations.
 module.exports = (env) => {
-  // Short circuit check `env` to avoid errors.
-  if (env && env.prod) {
-    const prod = require('./webpack.prod');
-    return merge.smart(build, prod);
-  } else {
-    return merge.smart(build, { mode: 'development' });
-  }
+  const isProduction = env && env.prod;
+  const isHotReload = env && env.hot && !isProduction;
+  return {
+    mode: isProduction ? 'production' : 'development',
+    output: {
+      filename: 'theme/static/[name].js'
+    },
+    module: {
+      rules: [
+        {
+          // Pass JS files through ESLint.
+          test: /\.js$/iu,
+          exclude: /node_modules/u,
+          loader: 'eslint-loader'
+        },
+        {
+          // Process SCSS and extract a CSS file.
+          test: /\.(?:css|scss|sass)$/iu,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+        },
+        {
+          // Load fonts.
+          test: /\.woff2?$/u,
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'theme/static/fonts',
+            publicPath: './fonts/'
+          }
+        }
+      ]
+    },
+    plugins: [
+      // Copies files to the dist/theme folder.
+      new CopyPlugin(
+        [
+          !isHotReload && { from: 'layouts', to: 'theme/layouts' },
+          { from: 'i18n', to: 'theme/i18n' },
+          { from: 'public', to: 'theme' }
+        ].filter(Boolean)
+      ),
+      // Extracts a CSS file.
+      new MiniCssExtractPlugin({
+        filename: 'theme/static/[name].css'
+      }),
+      // Extracts an RTL processed CSS file.
+      new WebpackRTLPlugin(),
+      // Lints stylesheets.
+      new StylelintPlugin({
+        syntax: 'scss'
+      })
+    ]
+  };
 };
