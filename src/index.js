74283698c2ba3533ac18ceec5b3772093f0778d7
Lior Kummer <LKummer41@gmail.com>
2021-06-30 21:08:23 +0300

    Add basic hamburger menu on small screens

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 import './style/index.scss';
+
+function openHamburgerMenu() {
+  console.log('open');
+  document
+    .getElementById('navbar-hamburger')
+    .setAttribute('aria-expanded', 'true');
+  document.getElementById('hamburger-menu').classList.add('l-sidemenu-active');
+  document.body.classList.add('l-sidemenu-noscroll');
+}
+
+function closeHamburgerMenu() {
+  console.log('close');
+  document
+    .getElementById('navbar-hamburger')
+    .setAttribute('aria-expanded', 'false');
+  document
+    .getElementById('hamburger-menu')
+    .classList.remove('l-sidemenu-active');
+  document.body.classList.remove('l-sidemenu-noscroll');
+}
+
+function toggleHamburgerMenu() {
+  const expanded =
+    document.getElementById('navbar-hamburger').attributes['aria-expanded']
+      .value === 'true';
+  if (!expanded) {
+    openHamburgerMenu();
+  } else {
+    closeHamburgerMenu();
+  }
+}
+
+const hamburgerButton = document.getElementById('navbar-hamburger');
+hamburgerButton.addEventListener('click', toggleHamburgerMenu);
